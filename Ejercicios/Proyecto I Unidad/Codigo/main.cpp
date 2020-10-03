89a223316a0b8fec3c35a051706b7cd39ff3a646
Karen Paz <mabelpazf@gmail.com>
2020-10-03 16:26:56 -0600

    Agregando proyecto1

---
+++ Ejercicios/Proyecto I Unidad/Codigo/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "startShip.h"
+#include "snake.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int opcion = 0;
+
+    while(true) {
+        system("cls");
+        cout << endl;
+
+        cout << " ******************** " << endl;
+        cout << "    MENU DE JUEGOS    " << endl;
+        cout << " ******************** " << endl << endl;
+        cout << " Seleccione un juego: " << endl << endl;
+
+        cout << " 1 - Nave estelar " << endl << endl;
+        cout << " 2 - Serpiente " << endl << endl;
+        cout << " 0 - Salir " << endl << endl;
+        cin >> opcion;
+
+        system("cls");
+        switch (opcion)
+        {
+        case 1: 
+            starShip();            
+            break;
+        case 2:
+            snake();
+            break;
+        default:
+            break;
+        }
+    
+        system("pause");
+        cout << endl;
+
+        if (opcion == 0) {
+            break;
+        }
+   }
+    return 0;
+}
